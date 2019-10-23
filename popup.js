7ae8f73cfc75b5072a5fb23aedf4b2658764bb74
Thomas Smith <thsm@umich.edu>
2019-10-22 23:02:25 -0400

    Add text input error messages

--- popup.js
+++ popup.js
@@ -1,8 +1,20 @@
 const videoIDEl = document.getElementById("videoID");
 const addEl = document.getElementById("add");
 
+const showInputError = () => {
+  videoIDEl.setCustomValidity("There was an error adding the video");
+};
+
 addEl.addEventListener("click", () => {
-  const idToAdd = videoIDEl.value;
-  storage.addToPlaylist(idToAdd);
-  videoIDEl.value = "";
+  const idToAdd = videoIDEl.value.trim();
+  if (idToAdd === "") {
+    showInputError();
+  }
+  else {
+    storage.addToPlaylist(idToAdd).then(() => {
+      videoIDEl.value = "";
+    }).catch(() => {
+      showInputError();
+    });
+  }
 });
