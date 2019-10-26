08e27ee245999a863146cc712fd5868c4a7e66aa
Thomas Smith <thsm@umich.edu>
2019-10-26 17:14:12 -0400

    Add Playlist abstract model

---
+++ util/playlist.js
@@ -0,0 +1,29 @@
+(function(global) {
+  class Playlist {
+    // REQUIRES: videoList.length !== 0
+    constructor(videoList) {
+      this.list = videoList;
+    }
+    videoAt(index) {
+      return this.list[index];
+    }
+  }
+  class PlaylistStatus {
+    // REQUIRES: indices.length <= playlist.list.length
+    //           indices has unique numbers in 0..playlist.list.length - 1
+    constructor(playlist, indices) {
+      this.playlist = playlist;
+      this.indices = indices;
+      this.index = 0;
+    }
+    currentVideo() {
+      return this.playlist.videoAt(this.playlist[this.index]);
+    }
+    nextVideo() {
+      this.index = (this.index + 1) % this.indices.length;
+    }
+  }
+
+  global.Playlist = Playlist;
+  global.PlaylistStatus = PlaylistStatus;
+})(this);
