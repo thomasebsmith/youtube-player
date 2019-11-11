082994191c3c7f806ac9ae961bb920c6e81bcaaa
Thomas Smith <thsm@umich.edu>
2019-11-10 23:49:39 -0500

    Add inline documentation to contentScript.js

--- contentScript.js
+++ contentScript.js
@@ -1,10 +1,16 @@
+// The full URL of the next video to play.
 let nextVideo = null;
+
+// Whether the video on this page has finished playing.
 let videoDone = false;
 
 const goToNextVideo = () => {
   location.replace(nextVideo);
 };
 
+// When a message is received from the background script, set nextVideo
+//  accordingly and play the video if the current video is already done or if
+//  the video should be force-played now.
 browser.runtime.onMessage.addListener(({playNextURL, forceNow}) => {
   nextVideo = playNextURL;
   if (videoDone || forceNow) {
@@ -12,6 +18,7 @@ browser.runtime.onMessage.addListener(({playNextURL, forceNow}) => {
   }
 });
 
+// If a <video> element exists on the page, go to the next video when it ends.
 const videoEl = document.querySelector("video");
 
 if (videoEl !== null) {
