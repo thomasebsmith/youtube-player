748b1b72cf62589d503ee2b64b84f363d6dde5d3
Thomas Smith <thsm@umich.edu>
2020-03-22 23:00:10 -0400

    Add random/in order selection for each playlist

--- options/options.js
+++ options/options.js
@@ -13,7 +13,31 @@ const createPlaylistEl = (playlist, id) => {
   nameEl.setAttribute("placeholder", "name");
   el.appendChild(nameEl);
 
+  const randomOrderEl = document.createElement("input");
+  randomOrderEl.setAttribute("type", "radio");
+  randomOrderEl.setAttribute("name", "ordering-" + id);
+  randomOrderEl.setAttribute("value", "random");
+  randomOrderEl.setAttribute("id", "randomRadio-" + id);
+  el.appendChild(randomOrderEl);
+  const randomOrderLabelEl = document.createElement("label");
+  randomOrderLabelEl.setAttribute("for", "randomRadio-" + id);
+  randomOrderLabelEl.textContent = "Random order";
+  el.appendChild(randomOrderLabelEl);
+
+  const inOrderEl = document.createElement("input");
+  inOrderEl.setAttribute("type", "radio");
+  inOrderEl.setAttribute("name", "ordering-" + id);
+  inOrderEl.setAttribute("value", "inOrder");
+  inOrderEl.setAttribute("id", "inOrderRadio-" + id);
+  el.appendChild(inOrderEl);
+  const inOrderLabelEl = document.createElement("label");
+  inOrderLabelEl.setAttribute("for", "inOrderRadio-" + id);
+  inOrderLabelEl.textContent = "In order";
+  el.appendChild(inOrderLabelEl);
+
   // TODO
+
+  formEl.appendChild(el);
 };
 
 const loadOptions = (options, playlists) => {
